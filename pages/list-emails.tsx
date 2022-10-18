bd03701200bf3a1f7781818bb90398d2d6ef6294
Theryston <fuktodo2@gmail.com>
2022-10-18 17:50:31 -0300

    fix: add list emails page

---
+++ pages/list-emails.tsx
@@ -0,0 +1,30 @@
+import { Box, Typography } from "@material-ui/core";
+import prisma from "../lib/database/prisma";
+
+export default function ListEmails({ emails }: any) {
+  return (
+    <>
+      <Box>
+        {emails.map((email: any) => (
+          <Typography key={email.id}>
+            {email.id} - {email.email}
+          </Typography>
+        ))}
+      </Box>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const emails = await prisma.email.findMany({
+    where: {
+      isVerified: true,
+    },
+  });
+
+  return {
+    props: {
+      emails: JSON.parse(JSON.stringify(emails)),
+    },
+  };
+}
