c0fedcbd8b1b11ff5f1591e74c5e86a2bd9a2677
Theryston <fuktodo2@gmail.com>
2022-10-18 17:09:28 -0300

    fix: remove redis

--- lib/services/requestFollow.ts
+++ lib/services/requestFollow.ts
@@ -1,29 +1,13 @@
 import axios from "axios";
-import Redis from "ioredis";
-
-const redis = new Redis(process.env.REDIS_URL || "");
 
 export async function requestFollows({ profileLink }: { profileLink: string }) {
-  try {
-    const { data } = await axios.get(`https://mundialmidia.com.br/api/v2`, {
-      params: {
-        key: process.env.GATEWAY_API_KEY,
-        action: "add",
-        service: "243",
-        link: profileLink,
-        quantity: "10",
-      },
-    });
-    if (data.error) {
-      console.log(data.error);
-      addQueue(profileLink);
-    }
-  } catch (error) {
-    console.log(error);
-    addQueue(profileLink);
-  }
+  await axios.get(`https://mundialmidia.com.br/api/v2`, {
+    params: {
+      key: process.env.GATEWAY_API_KEY,
+      action: "add",
+      service: "243",
+      link: profileLink,
+      quantity: "10",
+    },
+  });
 }
-
-const addQueue = async (profileLink: string) => {
-  await redis.lpush("profileLinks", profileLink);
-};
